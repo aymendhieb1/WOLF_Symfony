8881f6137ab969c270fbf67f7f117b1ceb3ca96a
youfes111 <131987778+youfes111@users.noreply.github.com>
2025-04-28 21:07:19 +0100

    seventh commit (adding voice regognation)

--- src/Controller/DashBoardController.php
+++ src/Controller/DashBoardController.php
@@ -6,27 +6,51 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
-use App\Repository\TodoRepository; // <-- ajouter ça
-
+use App\Repository\TodoRepository;
+use App\Repository\VehiculeRepository; // Inject VehicleRepository
 
 class DashBoardController extends AbstractController
 {
     #[Route('/dash/board', name: 'app_dash_board')]
-    public function index(UserRepository $userRepository, TodoRepository $todoRepository): Response // <-- injecter TodoRepository
+    public function index(UserRepository $userRepository, TodoRepository $todoRepository, VehiculeRepository $vehicleRepository): Response
     {
+
+        $vehicles = $vehicleRepository->findAll();
+
+        $cylinderCounts = [];
+        foreach ($vehicles as $vehicle) {
+            $cylinder = $vehicle->getCylinder();
+            if ($cylinder !== null) {
+                if (!isset($cylinderCounts[$cylinder])) {
+                    $cylinderCounts[$cylinder] = 0;
+                }
+                $cylinderCounts[$cylinder]++;
+            }
+        }
+        $vehicleCylinderStats = [
+            'labels' => array_map(function ($c) {
+                return $c . ' Cylinders';
+            }, array_keys($cylinderCounts)),
+            'data' => array_values($cylinderCounts),
+            'colors' => ['#4e73df', '#1cc88a', '#36b9cc', '#ff681a', '#f6c23e']
+        ];
         return $this->render('dash_board/index.html.twig', [
             'userCountRole2' => $userRepository->count(['role' => 2]),
             'userCountStatus1' => $userRepository->count(['status' => 1]),
             'userStatusStats' => [
                 'labels' => ['Comptes Actifs', 'Comptes Bloqués'],
                 'data' => [
-                    $userRepository->count(['status' => 0]), // Actifs
-                    $userRepository->count(['status' => 1])  // Bloqués
+                    $userRepository->count(['status' => 0]),
+                    $userRepository->count(['status' => 1])
                 ],
-                'colors' => [' #ff681a', '#132a3e'],
+                'colors' => ['#ff681a', '#132a3e'],
                 'borderColors' => ['#132a3e', '#ff681a']
             ],
-            'todos' => $todoRepository->findBy([], ['createdAt' => 'DESC']) // <-- ajouter la récupération des todos
+            'todos' => $todoRepository->findBy([], ['createdAt' => 'DESC']),
+            'vehicleCylinderStats' => $vehicleCylinderStats
+
+
         ]);
     }
 }
+
